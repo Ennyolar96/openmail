a2830cb2362896dce56f1f333d244a556e06b15e
ennyolar96 <ennyolar1996@gmail.com>
2026-04-28 07:49:26 +0100

    build

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -1,7 +1,3 @@
 import "reflect-metadata";
-export declare const createApp: () => {
-    app: import("express-serve-static-core").Express;
-    PORT: string | number;
-};
-export declare const app: import("express-serve-static-core").Express, PORT: string | number;
-export default app;
+import serverless from "serverless-http";
+export declare const handler: serverless.Handler;
--- dist/main.js
+++ dist/main.js
@@ -2,29 +2,22 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.PORT = exports.app = exports.createApp = void 0;
+exports.handler = void 0;
 require("reflect-metadata");
 const middleware_1 = require("@/global/middleware");
 const app_module_1 = require("@/app.module");
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
+const serverless_http_1 = __importDefault(require("serverless-http"));
 dotenv_1.default.config({ quiet: true });
-const createApp = () => {
-    const app = (0, express_1.default)();
-    (0, app_module_1.applicationMiddlewares)(app);
-    app.use(middleware_1.appErrorHandler);
-    const PORT = process.env.PORT || 5001;
-    return { app, PORT };
-};
-exports.createApp = createApp;
-_a = (0, exports.createApp)(), exports.app = _a.app, exports.PORT = _a.PORT;
-exports.default = exports.app;
-module.exports = exports.app;
+const app = (0, express_1.default)();
+(0, app_module_1.applicationMiddlewares)(app);
+app.use(middleware_1.appErrorHandler);
+const PORT = process.env.PORT || 5001;
 async function bootstrap() {
-    exports.app.listen(exports.PORT, () => {
-        console.log(`http://localhost:${exports.PORT}`);
+    app.listen(PORT, () => {
+        console.log(`http://localhost:${PORT}`);
     });
     const gracefulShutdown = async () => {
         console.log("Shutting down gracefully...");
@@ -38,4 +31,5 @@ async function bootstrap() {
 if (require.main === module) {
     void bootstrap();
 }
+exports.handler = (0, serverless_http_1.default)(app);
 //# sourceMappingURL=main.js.map
