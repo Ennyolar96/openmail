a871ca1520385d0fffc54e39738364f10cd2ce1b
ennyolar96 <ennyolar1996@gmail.com>
2026-04-28 08:49:13 +0100

    ....te

--- dist/routers/index.js
+++ dist/routers/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.applicationRouters = void 0;
-const smtp_controller_1 = require("@/app/email/smtp.controller");
+const smtp_controller_1 = require("../app/email/smtp.controller");
 const applicationRouters = (app) => {
     const smtpController = new smtp_controller_1.SmtpController();
 };
