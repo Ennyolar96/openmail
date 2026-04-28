f65cf9914bb6fa5eacb6405d9132501be5822e4f
ennyolar96 <ennyolar1996@gmail.com>
2026-04-28 07:53:30 +0100

    check

--- dist/main.js
+++ dist/main.js
@@ -5,8 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
 require("reflect-metadata");
-const middleware_1 = require("@/global/middleware");
-const app_module_1 = require("@/app.module");
+const middleware_1 = require("./global/middleware");
+const app_module_1 = require("./app.module");
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const serverless_http_1 = __importDefault(require("serverless-http"));
