c888d07ad302fbff086429fb2cbb272683915caa
ennyolar96 <ennyolar1996@gmail.com>
2026-04-28 08:32:49 +0100

    ..

--- dist/main.js
+++ dist/main.js
@@ -4,8 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
-const app_module_1 = require("@/app.module");
-const middleware_1 = require("@/global/middleware");
+const app_module_1 = require("./app.module");
+const middleware_1 = require("./global/middleware");
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 dotenv_1.default.config({ quiet: true });
