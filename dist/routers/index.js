a09ec4fc757eadfbe3f09802b2a1cbc7f8ab8191
ennyolar96 <ennyolar1996@gmail.com>
2026-04-28 11:07:48 +0100

    check this

--- dist/routers/index.js
+++ dist/routers/index.js
@@ -1,17 +1,77 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.applicationRouters = void 0;
-const input_1 = require("../app/email/input");
-const smtp_service_1 = require("../app/email/smtp.service");
 const middleware_1 = require("../global/middleware");
+const nodemailer = __importStar(require("nodemailer"));
+const p_limit_1 = __importDefault(require("p-limit"));
+const input_1 = require("../app/email/input");
+const config = (config) => {
+    const transporter = nodemailer.createTransport({
+        host: config.host,
+        port: config.port,
+        secure: config.secure,
+        auth: config.auth,
+        pool: true,
+        maxConnections: 5,
+    });
+    return transporter;
+};
 const applicationRouters = (app) => {
     app.post("/send-mail", (0, middleware_1.validateBody)(input_1.SendMailRequest), async (req, res, next) => {
+        const payload = req.body;
+        const limit = (0, p_limit_1.default)(5);
+        const mail = config(payload.config);
         try {
-            const result = await smtp_service_1.SmtpService.sendMail(req.body);
-            res.status(200).json(result);
         }
         catch (error) {
-            next(error);
+            res.status(500).json({
+                success: false,
+                message: "Failed to send email please try again",
+                data: {
+                    accepted: [],
+                    rejected: [],
+                    response: ["Failed to send email please try again"],
+                },
+            });
+        }
+        finally {
+            mail.close();
         }
     });
 };
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,19 +1,90 @@
-import { SendMailRequest } from "@/app/email/input";
+
 import { SmtpService } from "@/app/email/smtp.service";
 import { validateBody } from "@/global/middleware";
 import { Application, NextFunction, Request, Response } from "express";
+import * as nodemailer from "nodemailer";
+import pLimit from "p-limit";
+import { SendMailOutput, SendMailRequest } from "@/app/email/input";
+
+
+const config = (config: SendMailRequest["config"]): nodemailer.Transporter => {
+    const transporter = nodemailer.createTransport({
+        host: config.host,
+        port: config.port,
+        secure: config.secure,
+        auth: config.auth,
+        pool: true,
+        maxConnections: 5,
+    });
+
+    return transporter;
+}
 
 export const applicationRouters = (app: Application) => {
     app.post(
         "/send-mail",
         validateBody<SendMailRequest>(SendMailRequest),
         async (req: Request, res: Response, next: NextFunction) => {
+            const payload = req.body as SendMailRequest
+            const limit = pLimit(5);
+            const mail = config(payload.config);
+
             try {
-                const result = await SmtpService.sendMail(req.body as SendMailRequest);
-                res.status(200).json(result);
+                // Map over 'to' addresses and limit concurrency
+                // Wrapping with try/catch ensures we capture the 'to' address even on failure
+                // const task = payload.mail.to.map((to) => {
+                //   return limit(async () => {
+                //     try {
+                //       const result = await mail.sendMail({ ...payload.mail, to });
+                //       return { to, response: result.response as string };
+                //     } catch (error: any) {
+                //       return Promise.reject({
+                //         to,
+                //         reason: error?.message || "Unknown error",
+                //       });
+                //     }
+                //   });
+                // });
+
+                // const data = await Promise.allSettled(task);
+
+                // // TypeScript requires type predicates to narrow down union types from Promise.allSettled
+                // const rejected = data.filter(
+                //   (item): item is PromiseRejectedResult => item.status === "rejected",
+                // );
+                // const accepted = data.filter(
+                //   (
+                //     item,
+                //   ): item is PromiseFulfilledResult<{ to: string; response: string }> =>
+                //     item.status === "fulfilled",
+                // );
+
+                // return {
+                //   success: true,
+                //   message: "Email send successfully",
+                //   data: {
+                //     accepted: accepted.map((item) => item.value.to),
+                //     rejected: rejected.map((item) => item.reason.to),
+                //     response: accepted.map((item) => item.value.response),
+                //   },
+                // };
             } catch (error) {
-                next(error);
+                res.status(500).json({
+                    success: false,
+                    message: "Failed to send email please try again",
+                    data: {
+                        accepted: [],
+                        rejected: [],
+                        response: ["Failed to send email please try again"],
+                    },
+                });
+            } finally {
+                mail.close();
             }
-        },
-    );
+            //         } catch (error) {
+            //             next(error);
+            //         }
+            // },
+            // );
+        })
 }
